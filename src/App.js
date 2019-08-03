285863140e36c7ccdcbacb41bd93a3d8d6b839ac
nikosRepo <nikosrep@gmail.com>
2019-08-03 22:08:23 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("https://murmuring-harbor-70110.herokuapp.com:3000/image", {
+          fetch("https://murmuring-harbor-70110.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
